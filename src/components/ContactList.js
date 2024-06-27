51768ea10220c471587421ef6d69306d1cf91289
Tanvi Pooranmal Meena <96572616+TanviPooranmal@users.noreply.github.com>
2024-06-28 03:45:19 +0530

    Fix delete bug

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,14 +1,27 @@
 // src/components/ContactList.js
 import React, { useState } from 'react';
-import { db } from '../firebase';
+import { db, storage } from '../firebase';
 import { deleteDoc, doc } from 'firebase/firestore';
+import { ref, deleteObject } from 'firebase/storage';
 
 const ContactList = ({ contacts, fetchContacts, setCurrentContact }) => {
   const [loading, setLoading] = useState(false);
 
-  const handleDelete = async (id) => {
+  const handleDelete = async (contact) => {
     setLoading(true);
-    await deleteDoc(doc(db, 'contacts', id));
+
+    // Delete the image from storage
+    if (contact.imageURL) {
+      const imageRef = ref(storage, contact.imageURL);
+      try {
+        await deleteObject(imageRef);
+      } catch (error) {
+        console.error('Error deleting image:', error);
+      }
+    }
+
+    // Delete the document from Firestore
+    await deleteDoc(doc(db, 'contacts', contact.id));
     setLoading(false);
     fetchContacts();
   };
@@ -20,11 +33,11 @@ const ContactList = ({ contacts, fetchContacts, setCurrentContact }) => {
         {contacts.map(contact => (
           <div key={contact.id} className="contact-item">
             <div className="contact-avatar">
-          <img src={contact.imageURL} alt={contact.name} className="avatar-img" />
-        </div>
-        <h3 className="contact-name">{contact.name}</h3>
-        <p className="contact-info"><span role="img" aria-label="phone">📞</span> {contact.phone}</p>
-        <p className="contact-info"><span role="img" aria-label="email">📧</span> {contact.email}</p>
+              <img src={contact.imageURL} alt={contact.name} className="avatar-img" />
+            </div>
+            <h3 className="contact-name">{contact.name}</h3>
+            <p className="contact-info"><span role="img" aria-label="phone">📞</span> {contact.phone}</p>
+            <p className="contact-info"><span role="img" aria-label="email">📧</span> {contact.email}</p>
             <button
               onClick={() => setCurrentContact(contact)}
               className="edit-button"
@@ -33,7 +46,7 @@ const ContactList = ({ contacts, fetchContacts, setCurrentContact }) => {
               {loading ? 'Processing...' : 'Edit'}
             </button>
             <button
-              onClick={() => handleDelete(contact.id)}
+              onClick={() => handleDelete(contact)}
               className="delete-button"
               disabled={loading}
             >
